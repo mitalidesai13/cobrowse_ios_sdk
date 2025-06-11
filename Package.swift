80854bab08324359859d06261881c93d90a6f006
Mitali Desai <mitali@blitzz.co>
2025-06-11 13:19:03 +0530

    Add binary SDK v1.0.8

--- Package.swift
+++ Package.swift
@@ -3,25 +3,17 @@ import PackageDescription
 
 let package = Package(
     name: "BlitzzCobrowseSDK",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v12)], // or .v15 depending on your SDK minimum
     products: [
         .library(
             name: "BlitzzCobrowseSDK",
             targets: ["BlitzzCobrowseSDK"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/moozzyk/SignalR-Client-Swift.git", from: "1.1.0"),
-        .package(url: "https://github.com/valpackett/SwiftCBOR", branch: "master")
-    ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "BlitzzCobrowseSDK",
-            dependencies: [
-                .product(name: "SignalRClient", package: "SignalR-Client-Swift"),
-                .product(name: "SwiftCBOR", package: "SwiftCBOR")
-            ],
-            path: "Sources/BlitzzCobrowseSDK"
+            path: "BlitzzCobrowseSDK.xcframework" // <-- local path to the xcframework
         )
     ]
 )
