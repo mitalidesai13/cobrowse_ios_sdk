d8d103f103ef4dc1f3051f80ed51b029b27b69d7
Mitali Desai <mitali@blitzz.co>
2025-06-11 12:50:00 +0530

    Fix package structure and dependencies

--- Package.swift
+++ Package.swift
@@ -3,30 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "BlitzzCobrowseSDK",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "BlitzzCobrowseSDK",
-            targets: ["BlitzzCobrowseSDKWrapper"]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/moozzyk/SignalR-Client-Swift.git", from: "1.1.0"),
-        .package(url: "https://github.com/valpackett/SwiftCBOR", .branch("master"))
+            targets: ["BlitzzCobrowseSDK"])
     ],
     targets: [
         .binaryTarget(
-            name: "BlitzzCobrowseSDKBinary",
-            path: "BlitzzCobrowseSDK.xcframework"
-        ),
-        .target(
-            name: "BlitzzCobrowseSDKWrapper",
-            dependencies: [
-                .target(name: "BlitzzCobrowseSDKBinary"),
-                .product(name: "SignalRClient", package: "SignalR-Client-Swift"),
-                .product(name: "SwiftCBOR", package: "SwiftCBOR")
-            ],
-            path: "Sources"
+            name: "BlitzzCobrowseSDK",
+            path: "BlitzzCobrowseSDK.xcframework" // Path relative to Package.swift
         )
     ]
 )
